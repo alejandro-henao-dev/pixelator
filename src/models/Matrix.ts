e387ac8c075bd9b219c996bcc6db2e3b5e834a97
Alejandro Henao <ahenaoes@gmail.com>
2023-05-01 19:52:19 -0500

    add matrix class methods

--- src/models/Matrix.ts
+++ src/models/Matrix.ts
@@ -10,8 +10,48 @@ export class Matrix<T> {
     public cells: Array<Array<T>>,
     public size: Size
   ) { }
+
+  static fromSize<T>(size:Size): Matrix<T | null>{
+    const matrix:Array<Array<T | null>>=[]
+    for (let col = 0; col < size.height; col++) {
+      matrix.push([])
+      for (let row = 0; row < size.width; row++) {
+        matrix?.at(-1)?.push(null)
+      }  
+    }
+    return new Matrix<T | null>(matrix, size)
+  }
   
-  at(point:Point) {
-    return this.cells.at(point.x)?.at(point.y)
+  at(point: Point): T  {
+    if (point.x <= this.size.width && point.y <= this.size.height) {
+      const select=this.cells.at(point.y)?.at(point.x)
+      if (select) {
+        return select
+      }
+    }
+    throw new Error("point exceed the matrix size");
+  }
+  
+
+  getSoroundingMatrix(centerPoint: Point, threshold: number = 1): Matrix<T> {
+    console.log("subgrid", centerPoint)
+    if (threshold < 1) {
+      throw new Error("Surounding threshold should be at least 1")
+    }
+
+    const length = (threshold * 2) + 1
+    
+    const size=new Size(length, length)
+    const subMatrix: Array<Array<T>> = []
+    
+    for (let y = centerPoint.y - threshold; y <= centerPoint.y + threshold; y++) {
+      subMatrix.push([])
+      for (let x = centerPoint.x - threshold; x <= centerPoint.x + threshold; x++) {
+        subMatrix?.at(-1)?.push( (this.cells[y][x]) as any) 
+      
+      }  
+    }
+    
+    return new Matrix<T>(subMatrix, size)
   }
 }
